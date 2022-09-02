358c9de43b5c641470e42b1c5d84a58cbae381f1
asadullahislam <asadullah0072002@gmail.com>
2022-09-02 19:31:07 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,122 @@
+
+
+const loadPhones = async (searchText, dataLimit) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data, dataLimit);
+}
+const displayPhones = (phones, dataLimit) => {
+    const phonesContainer = document.getElementById('phones-container')
+    phonesContainer.textContent = '';
+    // display 10 phones only 
+    const showAll = document.getElementById('show-all');
+    if (dataLimit && phones.length > 10) {
+        phones = phones.slice(0, 10);
+        showAll.classList.remove('d-none');
+    }
+    else {
+        showAll.classList.add('d-none')
+    }
+    // phones = phones.slice(0, 10);
+
+
+
+
+    //display no phone found
+    const noPhone = document.getElementById('no-found-messege');
+    if (phones.length == 0) {
+        noPhone.classList.remove('d-none');
+    }
+    else {
+        noPhone.classList.add('d-none');
+    }
+
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        
+                <div class="col">
+                    <div class="card p-4">
+                        <img src=" ${phone.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"> ${phone.phone_name}</h5>
+                            <p class="card-text">This a card</p>
+                            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">phone Deltails</button>
+
+                            
+                        </div>
+                    </div>
+                </div>
+            
+        `
+        phonesContainer.appendChild(phoneDiv);
+
+        // stop loader
+
+    })
+    toggleSpinner(false);
+}
+
+const processSearch = (dataLimit) => {
+    toggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    loadPhones(searchText, dataLimit);
+}
+// handle search button click 
+document.getElementById('btn-search').addEventListener('click', function () {
+    //start loader
+    processSearch(10);
+
+    //dearch input field enter key handler
+    document.getElementById('search-field').addEventListener('keypress', function (e) {
+
+        if (e.key === 'Enter') {
+            processSearch(10);
+        }
+    });
+
+
+})
+
+const toggleSpinner = isLoading => {
+    const loaderSpinner = document.getElementById('loader');
+    if (isLoading) {
+        loaderSpinner.classList.remove('d-none');
+    }
+    else {
+        loaderSpinner.classList.add('d-none');
+    }
+}
+
+// not the best way to load show all
+document.getElementById('btn-show-all').addEventListener('click', function () {
+    processSearch();
+})
+
+const loadPhoneDetails = async id => {
+    const url = ` https://openapi.programming-hero.com/api/phone/${id}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone => {
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.innerHTML = `
+    <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No Release Date Found'}</p>
+    <p>Storage:${phone.mainFeatures.memory}</p>
+
+    `
+}
+
+
+
+
+// loadPhones(apple);
